6fe15eec6848920f7bce2b80f078a0c62f944b5c
Olena Zamyslova <olenazamyslova@gmail.com>
2023-02-11 01:09:59 +0200

    ready adaptive modal + sprite + new js file for modal in the main index.html file

---
+++ src/js/modal--team.js
@@ -0,0 +1,35 @@
+(() => {
+    const openModalBtn = document.querySelector('[data-modal-open]');
+    const closeModalBtn = document.querySelector('.team-modal__button');
+    const modal = document.querySelector('.modal-overlay');
+    const body = document.querySelector('body');
+  
+    openModalBtn.addEventListener('click', openModal);
+  
+    function openModal() {
+      modal.classList.remove('is-hidden');
+      window.addEventListener('keydown', onPressEscape);
+      closeModalBtn.addEventListener('click', closeModal);
+      modal.addEventListener('click', backdropCloseModal);
+      body.classList.add('scroll-hidden');
+      document.body.style.overflow = 'hidden';
+    }
+    function closeModal() {
+      body.classList.remove('scroll-hidden');
+      closeModalBtn.removeEventListener('click', closeModal);
+      modal.classList.add('is-hidden');
+      modal.removeEventListener('click', closeModal);
+      window.removeEventListener('keydown', onPressEscape);
+      document.body.style.overflow = 'visible';
+    }
+    function onPressEscape(event) {
+      if (event.code === 'Escape') {
+        closeModal();
+      }
+    }
+    function backdropCloseModal(event) {
+      if (event.currentTarget === event.target) {
+        closeModal();
+      }
+    }
+  })();
