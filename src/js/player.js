1f18f7850ac11945fc0f532bc7a8f19ce3e7a923
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2023-02-12 20:45:01 +0100

    fix

--- src/js/player.js
+++ src/js/player.js
@@ -7,39 +7,40 @@ const modal = document.querySelector('[data-modal]');
 modal.addEventListener('click', onClose);
 
 export default async function findMovieTrailer(id) {
-    try {
-      const response = await ApiService.getMovieTreiler(id);
-      const { data } = response;
-  
-      const videoKey = data.results.find(result => result.type === 'Trailer' && result.official).key;
-  
-      createPlayer(videoKey);
-      
-      window.addEventListener('keydown', onEscPress)
-  
-    } catch (error) {
-      console.log(error);
-      return Notify.failure('Sorry, there no trailer to this film!');
-    }
+  try {
+    const response = await ApiService.getMovieTreiler(id);
+    const { data } = response;
+
+    const videoKey = data.results.find(
+      result => result.type === 'Trailer' && result.official
+    ).key;
+
+    createPlayer(videoKey);
+
+    window.addEventListener('keydown', onEscPress);
+  } catch (error) {
+    console.log(error);
+    return Notify.failure('Sorry, there no trailer to this film!');
   }
-  
-  function createPlayer (id) {
-    const frame = `<iframe class="player__frame" id="player" type="text/html"
-    src="http://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+}
+
+function createPlayer(id) {
+  const frame = `<iframe class="player__frame" id="player" type="text/html"
+    src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
   allowfullscreen></iframe>`;
-    player.innerHTML = frame;
-  }
+  player.innerHTML = frame;
+}
 
-  function onEscPress(e) {
-    if (e.key !== 'Escape') {
-      return
-    }
+function onEscPress(e) {
+  if (e.key !== 'Escape') {
+    return;
+  }
+  player.innerHTML = '';
+  window.removeEventListener('keydown', onEscPress);
+}
+
+function onClose(e) {
+  if (e.target !== player) {
     player.innerHTML = '';
-    window.removeEventListener('keydown', onEscPress)
   }
-
-  function onClose(e) {
-    if (e.target !== player) {
-      player.innerHTML = '';
-    }
-  }
+}
